fc3ea2b9ea8cac293c2e3008748ee2df3b933c15
André Faria <andrefaria-1@hotmail.com>
2020-07-04 16:44:50 -0300

    Service layer, component registration

--- src/main/java/com/educandoweb/course/resources/UserResource.java
+++ src/main/java/com/educandoweb/course/resources/UserResource.java
@@ -1,19 +1,35 @@
 package com.educandoweb.course.resources;
 
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import java.util.List;
 
 import com.educandoweb.course.entities.User;
+import com.educandoweb.course.services.UserService;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping(value = "/users")
 public class UserResource {
 
+	@Autowired
+	private UserService service;
+
 	@GetMapping
-	public ResponseEntity<User> findAll() { 
-		User u = new User(1L, "Andre", "andre@gmail.com", "3333333", "123456789");
-		return ResponseEntity.ok().body(u);
+	public ResponseEntity<List<User>> findAll() {
+		List<User> list = service.findAll();
+		return ResponseEntity.ok().body(list);
+	}
+
+	@GetMapping(value = "/{id}") // indica que a requisicao aceita um id como parametro.
+	public ResponseEntity<User> findById(@PathVariable Long id) { // pega o parametro passado para a requisicao
+		User obj = service.findById(id);
+		return ResponseEntity.ok().body(obj);
+
 	}
 }
+
