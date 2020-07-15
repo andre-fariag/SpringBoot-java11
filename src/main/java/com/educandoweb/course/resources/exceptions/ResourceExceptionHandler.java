315c2e30bd3b072e966775daee2fd2b5322271b0
André Faria <andrefaria-1@hotmail.com>
2020-07-15 15:22:44 -0300

    Exception handling - delete

--- src/main/java/com/educandoweb/course/resources/exceptions/ResourceExceptionHandler.java
+++ src/main/java/com/educandoweb/course/resources/exceptions/ResourceExceptionHandler.java
@@ -2,14 +2,15 @@ package com.educandoweb.course.resources.exceptions;
 
  import java.time.Instant;
 
- import javax.servlet.http.HttpServletRequest;
+	import javax.servlet.http.HttpServletRequest;
 
- import org.springframework.http.HttpStatus;
- import org.springframework.http.ResponseEntity;
- import org.springframework.web.bind.annotation.ControllerAdvice;
- import org.springframework.web.bind.annotation.ExceptionHandler;
+	import com.educandoweb.course.services.exceptions.DatabaseException;
+	import com.educandoweb.course.services.exceptions.ResourceNotFoundException;
 
- import com.educandoweb.course.services.exceptions.ResourceNotFoundException;
+	import org.springframework.http.HttpStatus;
+	import org.springframework.http.ResponseEntity;
+	import org.springframework.web.bind.annotation.ControllerAdvice;
+	import org.springframework.web.bind.annotation.ExceptionHandler;
 
  @ControllerAdvice
  public class ResourceExceptionHandler {
@@ -20,6 +21,16 @@ package com.educandoweb.course.resources.exceptions;
  		HttpStatus status = HttpStatus.NOT_FOUND;
  		StandardError err = new StandardError(Instant.now(), status.value(), error, e.getMessage(), request.getRequestURI());
  		return ResponseEntity.status(status).body(err);
+	 }
+	 
+	 @ExceptionHandler(DatabaseException.class)
+ 	public ResponseEntity<StandardError> resourceNotFound(DatabaseException e, HttpServletRequest request) {
+ 		String error = "Database error";
+ 		HttpStatus status = HttpStatus.BAD_REQUEST;
+ 		StandardError err = new StandardError(Instant.now(), status.value(), error, e.getMessage(), request.getRequestURI());
+ 		return ResponseEntity.status(status).body(err);
  	}
- }
+ }	
+
+
  
--- src/main/java/com/educandoweb/course/services/UserService.java
+++ src/main/java/com/educandoweb/course/services/UserService.java
@@ -5,9 +5,12 @@ import java.util.Optional;
 
 import com.educandoweb.course.entities.User;
 import com.educandoweb.course.repositories.UserRepository;
+import com.educandoweb.course.services.exceptions.DatabaseException;
 import com.educandoweb.course.services.exceptions.ResourceNotFoundException;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 @Service // registra a classe como um componente do Spring
@@ -26,15 +29,21 @@ public class UserService {
         return obj.orElseThrow(() -> new ResourceNotFoundException(id)); // erro "condicional".
     }
 
-    public User insert (User obj){
+    public User insert(User obj) {
         return repository.save(obj);
     }
 
-    public void delete(Long id){
-        repository.deleteById(id);
+    public void delete(Long id) {
+        try {
+            repository.deleteById(id);
+        } catch (EmptyResultDataAccessException e) {
+            throw new ResourceNotFoundException(id);
+        } catch (DataIntegrityViolationException e){
+            throw new DatabaseException(e.getMessage());
+        }
     }
 
-    public User update(Long id, User obj){
+    public User update(Long id, User obj) {
         User entity = repository.getOne(id); // pega os dados sem acionar o banco de dados (mais eficiente)
         updateData(entity, obj);
         return repository.save(entity);
